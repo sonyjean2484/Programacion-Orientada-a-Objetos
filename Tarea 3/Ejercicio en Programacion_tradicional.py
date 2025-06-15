d2bd4f92b8a05ea0bb0909095713e624231d5502
sonyjean2484 <sj.vasquezp@uea.edu.ec>
2025-06-15 12:56:36 -0500

    P. Tradicional vs. POO

---
+++ Tarea 3/Ejercicio en Programacion_tradicional.py
@@ -0,0 +1,29 @@
+#Programación Tradicional
+#Ejercicio que realiza el promedio semanal de temperaturas ingresadas por el usuario
+
+# Variables globales
+temperaturas = []
+semana=["Lunes","Martes","Miercoles","Jueves","Viernes","Sábado","Domingo"]
+
+# Función para ingresar temperaturas diarias
+def ingresar_temperaturas():
+    global temperaturas
+    global semana
+    print("\nIngrese la temperatura de cada día de la semana")
+    for i in range(len(semana)):
+        temp = float(input(f"\tDía {semana[i]}: "))
+        temperaturas.append(temp)
+
+# Función para calcular el promedio semanal
+def calcular_promedio():
+    global temperaturas
+    if len(temperaturas) == 0:
+        print("No hay temperaturas registradas.")
+        return
+    promedio = sum(temperaturas) / len(temperaturas)
+    print(f"Promedio semanal del clima: {promedio:.2f}°C")
+
+# Programa principal
+print("--------TEMPERATURA PROMEDIO SEMANAL--------")
+ingresar_temperaturas() #uso de funciones en programación tradicioanl
+calcular_promedio()
